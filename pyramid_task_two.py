0cb4465f1557ccb8350b77d2faa6b564ac0c9c3a
arpitasingh133 <97514202+arpitasingh133@users.noreply.github.com>
2024-06-20 10:51:38 +0530

    Add files via upload

---
+++ pyramid_task_two.py
@@ -0,0 +1,9 @@
+def print_pyramid(rows):
+    for i in range(1, rows + 1):
+        print(" " * (rows - i), end="")
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+rows = 5
+print_pyramid(rows)
